ad474907297e0de6110fbcee7b066e13d250363c
Neus Agüera Valls <naguera@Neuss-MacBook-Air.local>
2020-03-13 11:31:55 +0100

    Updates

---
+++ precache-manifest.9421564786386d39e7df35705adaedd0.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4731b164854688c78c9ba4ac1780286a",
+    "url": "/cv-interactive/index.html"
+  },
+  {
+    "revision": "d6ec3ddcd6137327697f",
+    "url": "/cv-interactive/static/css/main.30d1ed2a.chunk.css"
+  },
+  {
+    "revision": "008021f2b9b644875ded",
+    "url": "/cv-interactive/static/js/2.be3f1817.chunk.js"
+  },
+  {
+    "revision": "d891fcc30dfc707e593ab65e09f6fef9",
+    "url": "/cv-interactive/static/js/2.be3f1817.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d6ec3ddcd6137327697f",
+    "url": "/cv-interactive/static/js/main.a43b132c.chunk.js"
+  },
+  {
+    "revision": "a649d6cb6b305d8f625e",
+    "url": "/cv-interactive/static/js/runtime-main.c42bc1eb.js"
+  },
+  {
+    "revision": "527d3ddfcacd6f0e02317f734f0d0db4",
+    "url": "/cv-interactive/static/media/cloud.527d3ddf.svg"
+  },
+  {
+    "revision": "c2a747d956c97b38e247e0fbc8ef6885",
+    "url": "/cv-interactive/static/media/cloudman.c2a747d9.svg"
+  },
+  {
+    "revision": "96ac909d5e4a84c5477ae956d6e0fc4e",
+    "url": "/cv-interactive/static/media/clover.96ac909d.svg"
+  },
+  {
+    "revision": "bf4dbc3d2d995576792093b32f64d3c4",
+    "url": "/cv-interactive/static/media/diamond.bf4dbc3d.svg"
+  },
+  {
+    "revision": "6edc5e00b0d28888e0bc7f1bea87d005",
+    "url": "/cv-interactive/static/media/heart.6edc5e00.svg"
+  },
+  {
+    "revision": "b7a2eda7d010cf7210fe0b4ef5b9f652",
+    "url": "/cv-interactive/static/media/paratrooper.b7a2eda7.svg"
+  },
+  {
+    "revision": "43d15c13c18e3257a36fdd6e3a023be8",
+    "url": "/cv-interactive/static/media/skater.43d15c13.svg"
+  },
+  {
+    "revision": "ae1fdbbcf1aab3fd156cf8385d1efb44",
+    "url": "/cv-interactive/static/media/spades.ae1fdbbc.svg"
+  },
+  {
+    "revision": "1bdfcd55c9f81583a4aa100e9874a3bd",
+    "url": "/cv-interactive/static/media/thinker.1bdfcd55.svg"
+  }
+]);
