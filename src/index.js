3b6ceb2ab1336c8b8af81907b7bbe76463cb3b8b
Neus Agüera Valls <naguera@Neuss-MacBook-Air.local>
2020-03-13 13:58:04 +0100

    change host name

--- src/index.js
+++ src/index.js
@@ -6,16 +6,18 @@ import * as serviceWorker from './serviceWorker';
 import {BrowserRouter as Router, Route} from "react-router-dom";
 
 
-  ReactDOM.render(
+  /*ReactDOM.render(
     <Router>
         <div>
           <Route exact path="/" component={RenderPage} />
         </div>
     </Router>,
     document.getElementById('root')
-  )
+  )*/
 
+  ReactDOM.render(RenderPage, document.getElementById('root'));
 
+  
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
