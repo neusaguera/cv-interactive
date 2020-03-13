af691f73d2120e3381c7b1da625d1c976fa321bd
Neus Agüera Valls <naguera@Neuss-MacBook-Air.local>
2020-03-13 12:18:19 +0100

    Updates

---
+++ precache-manifest.c29c2bcebf83ebe398a2ba8291e6c60b.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d1cfd52ac9b6bf0ce58cc1382cd6b50",
+    "url": "/index.html"
+  },
+  {
+    "revision": "06d7ea16e3c2c771a79c",
+    "url": "/static/css/main.30d1ed2a.chunk.css"
+  },
+  {
+    "revision": "d097967d50ced4fc8d61",
+    "url": "/static/js/2.8c0879a9.chunk.js"
+  },
+  {
+    "revision": "98061d712500f07672e3581cfdcd5a24",
+    "url": "/static/js/2.8c0879a9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "06d7ea16e3c2c771a79c",
+    "url": "/static/js/main.d52c123b.chunk.js"
+  },
+  {
+    "revision": "9c50e4f36d8613b15c21",
+    "url": "/static/js/runtime-main.994d540b.js"
+  },
+  {
+    "revision": "527d3ddfcacd6f0e02317f734f0d0db4",
+    "url": "/static/media/cloud.527d3ddf.svg"
+  },
+  {
+    "revision": "c2a747d956c97b38e247e0fbc8ef6885",
+    "url": "/static/media/cloudman.c2a747d9.svg"
+  },
+  {
+    "revision": "96ac909d5e4a84c5477ae956d6e0fc4e",
+    "url": "/static/media/clover.96ac909d.svg"
+  },
+  {
+    "revision": "bf4dbc3d2d995576792093b32f64d3c4",
+    "url": "/static/media/diamond.bf4dbc3d.svg"
+  },
+  {
+    "revision": "6edc5e00b0d28888e0bc7f1bea87d005",
+    "url": "/static/media/heart.6edc5e00.svg"
+  },
+  {
+    "revision": "b7a2eda7d010cf7210fe0b4ef5b9f652",
+    "url": "/static/media/paratrooper.b7a2eda7.svg"
+  },
+  {
+    "revision": "43d15c13c18e3257a36fdd6e3a023be8",
+    "url": "/static/media/skater.43d15c13.svg"
+  },
+  {
+    "revision": "ae1fdbbcf1aab3fd156cf8385d1efb44",
+    "url": "/static/media/spades.ae1fdbbc.svg"
+  },
+  {
+    "revision": "1bdfcd55c9f81583a4aa100e9874a3bd",
+    "url": "/static/media/thinker.1bdfcd55.svg"
+  }
+]);
